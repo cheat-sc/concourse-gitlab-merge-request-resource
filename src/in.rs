963d34e3ce7779050ad8fe5a0d691853afc82478
Shohei Maruyama <cheat.sc.linux@outlook.com>
2024-04-30 00:40:46 +0900

    rustfmt: Add rustfmt.toml
    This commit adds rustfmt.toml.
    
    Signed-off-by: Shohei Maruyama <cheat.sc.linux@outlook.com>

--- src/in.rs
+++ src/in.rs
@@ -1,16 +1,37 @@
 mod common;
-use common::*;
-use git2::build::{ RepoBuilder };
-use git2::{ RemoteCallbacks, FetchOptions, Cred, Oid };
-use std::path::Path;
-use gitlab::Gitlab;
-use gitlab::api::{ projects::{ self, merge_requests }, Query };
-use url::Url;
-use std::{ io, fs::File };
-use serde_json;
-use serde::{ Deserialize, Serialize };
+use anyhow::{
+	anyhow,
+	Context,
+	Result,
+};
 use clap::Parser;
-use anyhow::{ Result, anyhow, Context };
+use common::*;
+use git2::build::RepoBuilder;
+use git2::{
+	Cred,
+	FetchOptions,
+	Oid,
+	RemoteCallbacks,
+};
+use gitlab::api::{
+	projects::{
+		self,
+		merge_requests,
+	},
+	Query,
+};
+use gitlab::Gitlab;
+use serde::{
+	Deserialize,
+	Serialize,
+};
+use serde_json;
+use std::path::Path;
+use std::{
+	fs::File,
+	io,
+};
+use url::Url;
 
 #[derive(Debug, Deserialize)]
 struct Params {
@@ -21,7 +42,7 @@ struct Params {
 struct ResourceInput {
 	version: Option<Version>,
 	source: Source,
-	params: Option<Params>
+	params: Option<Params>,
 }
 
 #[derive(Debug, Serialize)]
@@ -39,13 +60,11 @@ struct Args {
 fn main() -> Result<()> {
 	let args = Args::parse();
 
-	let input: ResourceInput = get_data_from(&mut io::stdin()).map_err(|err| anyhow!("{}", err.downcast::<serde_json::Error>().unwrap()))?;
+	let input: ResourceInput =
+		get_data_from(&mut io::stdin()).map_err(|err| anyhow!("{}", err.downcast::<serde_json::Error>().unwrap()))?;
 
 	let uri = Url::parse(&input.source.uri)?;
-	let client = Gitlab::new(
-		uri.host_str().unwrap(),
-		&input.source.private_token,
-	)?;
+	let client = Gitlab::new(uri.host_str().unwrap(), &input.source.private_token)?;
 
 	let version = input.version.unwrap();
 
@@ -63,10 +82,19 @@ fn main() -> Result<()> {
 	let output = ResourceOutput {
 		version: version,
 		metadata: vec![
-			Metadata { name: "url".to_owned(), value: mr.web_url },
-			Metadata { name: "author".to_owned(), value: mr.author.name },
-			Metadata { name: "title".to_owned(), value: mr.title },
-		]
+			Metadata {
+				name: "url".to_owned(),
+				value: mr.web_url,
+			},
+			Metadata {
+				name: "author".to_owned(),
+				value: mr.author.name,
+			},
+			Metadata {
+				name: "title".to_owned(),
+				value: mr.title,
+			},
+		],
 	};
 
 	println!("{}", serde_json::to_string_pretty(&output)?);
@@ -85,29 +113,28 @@ fn main() -> Result<()> {
 	} {
 		eprintln!("Cloning repository...");
 		let mut cb = RemoteCallbacks::new();
-		cb.credentials(|_, _, _| {
-			Cred::userpass_plaintext("oauth2", &input.source.private_token)
-		});
+		cb.credentials(|_, _, _| Cred::userpass_plaintext("oauth2", &input.source.private_token));
 
 		let mut fo = FetchOptions::new();
 		fo.remote_callbacks(cb);
 
 		let mut builder = RepoBuilder::new();
-		let repo = builder.fetch_options(fo)
+		let repo = builder
+			.fetch_options(fo)
 			.branch(&mr.source_branch)
 			.clone(&project.http_url_to_repo, Path::new(&args.directory))
 			.with_context(|| anyhow!("failed to clone repository"))?;
 		repo.reset(
 			&repo.find_object(Oid::from_str(&mr.sha).unwrap(), None).unwrap(),
 			git2::ResetType::Hard,
-			None
-		).with_context(|| anyhow!("failed to checkout {}", &mr.sha))?;
+			None,
+		)
+		.with_context(|| anyhow!("failed to checkout {}", &mr.sha))?;
 	}
 
 	/* Dump version to a file for out */
-	let file = File::create(
-		Path::new(&args.directory).join(".merge-request.json")
-	).with_context(|| anyhow!("failed to create `.merge-request.json`"))?;
+	let file = File::create(Path::new(&args.directory).join(".merge-request.json"))
+		.with_context(|| anyhow!("failed to create `.merge-request.json`"))?;
 	serde_json::to_writer_pretty(file, &output.version)?;
 	Ok(())
 }
